9654a53e24ac00157e0913325bdcc13bb6751bb5
Dzmitry Malyshau <kvarkus@gmail.com>
2015-03-27 00:52:49 -0400

    Implemented scene ViewInfo

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 extern crate cgmath;
 extern crate gfx;
 extern crate gfx_phase;
+extern crate gfx_scene;
 
 pub mod forward;
 
@@ -17,5 +18,19 @@ impl<S: cgmath::BaseFloat> gfx_phase::ToDepth for ViewInfo<S> {
     type Depth = S;
     fn to_depth(&self) -> S {
         self.mx_vertex.w.z / self.mx_vertex.w.w
+    }
+}
+
+impl<
+    S: cgmath::BaseFloat + 'static,
+    T: cgmath::CompositeTransform3<S, cgmath::Quaternion<S>>, //TODO
+> gfx_scene::ViewInfo<S, T> for ViewInfo<S> {
+    fn new(mvp: cgmath::Matrix4<S>, view: T, _model: T) -> ViewInfo<S> {
+        use cgmath::ToMatrix3;
+        let (_, rot, _) = view.decompose();
+        ViewInfo {
+            mx_vertex: mvp,
+            mx_normal: rot.to_matrix3(),
+        }
     }
 }
